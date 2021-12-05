9ca0854b2f06b2f44c4cf7d742104a020ebc6edf
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-06 03:23:54 +0400

    Update apiServer.py

--- api/apiServer.py
+++ api/apiServer.py
@@ -1,7 +1,6 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/api/')
 def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    return Response("<h1>Flask</h1><p>Hi</p>", mimetype="text/html")
