dd3eb8cf13ad64463874d85ec992ad3738f60d5d
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-06 03:30:25 +0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,5 +2,5 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route('/api/')
-def catch_all(path):
+def catch_all():
     return Response("<h1>Flask</h1><p>Hi</p>", mimetype="text/html")
