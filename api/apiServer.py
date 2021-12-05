dd6aba078be02910ee269be0cab7add359859206
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-06 03:06:30 +0400

    Update apiServer.py

--- api/apiServer.py
+++ api/apiServer.py
@@ -1,6 +1,11 @@
-from flask import Flask, escape
-app = Flask(__name__)
+from flask import Flask, escape, Response
+from flask_restful import Resource, Api, reqparse
 
-class getProducts():
-    def hi_GET(self):
-        return "hi"
+app1 = Flask(__name__)
+app = Api(app)
+
+class getProducts(Resource):
+    def get(self):
+        return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+app.add_resource(getProducts, '/api/getproducts')
