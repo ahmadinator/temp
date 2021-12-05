90043b01ed37b5970defb865d3e2b590722d190d
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-05 20:36:26 +0400

    Update apiServer.py

--- api/apiServer.py
+++ api/apiServer.py
@@ -1,32 +1,6 @@
 from flask import Flask, escape
-from flask_cors import CORS
-from flask_restful import Resource, Api, reqparse
-from waitress import serve
-import sqlite3
-import json
-import os
-
 app = Flask(__name__)
-CORS(app)
-api = Api(app)
 
 class getProducts(Resource):
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('max', type=int, help='Max products to fetch')
-        args = parser.parse_args()
-        with open(os.getcwd() + '\\api\\db\\products.json') as json_file:
-            products = json.load(json_file)
-            if not args['max'] is None:
-                items = []
-                for i in range(args['max']):
-                    iS = str(i)
-                    if iS in products:
-                        items.append(products[iS])
-                return items
-            else:
-                return products
-
-api.add_resource(getProducts, '/api/getproducts')
-
-serve(app, host='0.0.0.0', port=8080)
+        return "hi"
