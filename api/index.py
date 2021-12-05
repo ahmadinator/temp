14e345ec00fff132b76facd3af8cccb79eefc173
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-06 03:47:01 +0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
-from flask import Flask, escape
-from flask_restful import Resource, Api, reqparse
+from flask import Flask
+from flask_restful import Resource, Api
 
 app1 = Flask(__name__)
 app = Api(app1)
 
-class getProducts(Resource):
+class test(Resource):
     def get(self):
         return "hi"
-app.add_resource(getProducts, '/api/')
+    
+app.add_resource(test, '/api/')
