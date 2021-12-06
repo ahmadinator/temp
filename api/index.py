f73ce3f6b423b8071ac525dd9706d1e0ee308700
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-06 17:31:00 +0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/api/getproducts")
+@app.route("/api/getproducts/")
 def productsA():
     return "products"
 
-@app.route("/api/lol")
+@app.route("/api/lol/")
 def lolxd():
     return "lol??"
