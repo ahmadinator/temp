6733b4c59bca78435feb7deb051dea419ec88495
Ahmad <41236167+ahmadinator@users.noreply.github.com>
2021-12-06 17:09:08 +0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from flask_restful import Resource, Api
 
-app1 = Flask(__name__)
-app = Api(app1)
+app = Flask(__name__)
 
-class test(Resource):
-    def get(self):
-        return "hi"
-    
-app.add_resource(test, '/api/')
+@app.route("/api/")
+def get(self):
+    return "hi"
